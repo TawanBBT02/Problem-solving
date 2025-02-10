d6e61c9489b1927bf928db5df165c2bfab2e2a00
TawanBBT02 <t0985595716@gmail.com>
2025-02-10 15:52:34 +0700

    hash

---
+++ Hash-/test.py
@@ -0,0 +1,15 @@
+data = [25,27,14,48,37,30,26,41,31,43,54,40]
+for i in  data:
+    print(i%len(data),end=" ")
+print()
+    
+result = []
+for i in range(len(data)):
+    res = []
+    for j in data:
+        
+        if j%12 == i:
+            res.append(j)
+    result.append(res)
+    
+print(result)
--- test.py
+++
@@ -1,5 +0,0 @@
-sum = 0
-for i in range(1,10):
-    if i % 3 == 0:
-        sum += i 
-print(sum)
