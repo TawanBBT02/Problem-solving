8a9444f9359f55bf9c3ddbef6b79fb8e42be1f27
TawanBBT02 <t0985595716@gmail.com>
2025-03-23 12:22:15 +0700

    Mul-Logis

--- STAT/finalStat/Simple_linear.py
+++ STAT/finalStat/Simple_linear.py
@@ -2,7 +2,7 @@ import random as rd
 import math
 D1 = [[24, 30, 26, 15, 16, 27, 30, 15, 24, 27], [20, 22, 13, 22, 27, 15, 15, 22, 22, 17]]
 D2 = [[38, 39, 18, 22, 26, 30, 28, 42, 25, 38], [24, 40, 23, 26, 16, 17, 34, 27, 26, 29]]
-data = D1
+data = D2
 def sim_table():
     data = []
     for  k in range(2):
@@ -53,3 +53,11 @@ def a(data):
     return result
 
 print(f"Y = {format(a(data), '.2f')} + {format(b(data), '.3f')}X ")
+
+print("X = ",sum(data[0]))
+print("Y = ",sum(data[1]))
+print("X^2 = ",sum([i**2 for i in data[0]]))
+print("Y^2 = ",sum([i**2 for i in data[1]]))
+print("XY = ",sum(data[0][i]*data[1][i] for i in range(len(data[0]))))
+print(f"S.D.X = {format(standard_deviation(data[0]), '.4f')}")
+print(f"S.D.Y = {format(standard_deviation(data[1]), '.4f')}")
